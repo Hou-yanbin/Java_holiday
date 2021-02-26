3ba48332a10e51d2f32fcf883519173304fe0074
Jack hou <256595662@qq.com>
2021-02-26 13:50:44 +0800

    Test2测试

---
+++ src/AbstractExer/Test1/Test2.java
@@ -0,0 +1,13 @@
+package AbstractExer.Test1;
+
+/**
+ * @version v-1.8.0_131.
+ * @auther Jack hou  Email:256595662@qq.com
+ * @data 2021/2/26/13:48
+ * @Description:
+ **/
+public class Test2 {
+    public static void main(String[] args) {
+        System.out.println("hello test");
+    }
+}
